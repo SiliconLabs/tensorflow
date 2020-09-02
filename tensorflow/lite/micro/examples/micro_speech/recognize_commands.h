223d46d0da4e2ef2d5cac45e6a9878e4defbe436
Andras Fekete <anfekete@silabs.com>
2020-09-02 15:33:06 -0400

    Optimize to get better recognition

--- tensorflow/lite/micro/examples/micro_speech/recognize_commands.h
+++ tensorflow/lite/micro/examples/micro_speech/recognize_commands.h
@@ -131,10 +131,10 @@ class RecognizeCommands {
   // further recognitions for a set time after one has been triggered, which can
   // help reduce spurious recognitions.
   explicit RecognizeCommands(tflite::ErrorReporter* error_reporter,
-                             int32_t average_window_duration_ms = 1000,
-                             uint8_t detection_threshold = 200,
-                             int32_t suppression_ms = 1500,
-                             int32_t minimum_count = 3);
+                             int32_t average_window_duration_ms = 700,
+                             uint8_t detection_threshold = 130,
+                             int32_t suppression_ms = 1000,
+                             int32_t minimum_count = 2);
 
   // Call this with the results of running a model on sample data.
   TfLiteStatus ProcessLatestResults(const TfLiteTensor* latest_results,
